bb3f33fa143167d091bfe48ee05fbcb8d536f55c
Jevinus <jevinus@protonmail.com>
2024-02-28 17:46:33 -0500

    Updated button appearence

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-const gridContainer = document.querySelector("grid-container")
-const gridSize = 16;
+// Create grid elements in js for functionality
+// Create a button allowing for grid adjustment ranging from 1-100
+// Create a button allowing for the change of grid color using RGB
+// Add an onclick for the grid divs that changes individual its color 
+
+const grid = document.createElement('div');
+grid.className='grid';
+grid.style.backgroundColor='grey';
+grid.style.border='1px solid black';
+grid.style.visibility='visible'
+let gridCount = Math.floor.grid() * 256;
+
+// Grid 1-100 button
+const selector = document.createElement('grid_selector');
+
+function gridScaler() {
+  let input = prompt('Input 1-100');
+  let inputConf = confirm('Confirm or Deny')
+}
+
+gridScaler()
