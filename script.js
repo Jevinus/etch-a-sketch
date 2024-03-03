ca2ff29359c110afea86912cb124dad9feb77da4
Jevinus <jevinus@protonmail.com>
2024-03-02 23:11:28 -0500

    Added RGB function and  grid function

--- script.js
+++ script.js
@@ -3,14 +3,37 @@
 // Create a button allowing for the change of grid color using RGB
 // Add an onclick for the grid divs that changes individual its color 
 
-const grid = document.createElement('div');
-grid.classList.add('grid')
-const gridContainer = document.getElementById('grid-container')
-grid.style.backgroundColor = 'gray';
-grid.style.border = '2px solid black';
-grid.style.height = ' ';
+// Dynamic grid function allowing for 
+function grid(input) {
+  let container = document.getElementById('grid_container');
+  let gridInput = input;
+  prompt(gridInput);
 
-gridContainer.appendChild(grid);
+  for (let i = 0; i <= input; i++) {
+    let grid = document.createElement('div');
+    grid.className = 'grid';
+    grid.style.backgroundColor = 'blue';
+    grid.style.border = '2px solid black';
+    grid.style.overflow = 'hidden';
+    container.appendChild(grid);
+  }
 
-// Grid adjustment function
+  container.appendChild(grid_container);
+}
+
+// Button to change grid size
+let gridButton = document.getElementsByClassName('grid_selector');
+gridButton.addEventListener('click', grid())
+
+// RGB value generator for grid cells
+function gridRGB() {
+  const x = Math.floor(Math.random() * 256);
+  const y = Math.floor(Math.random() * 256);
+  const z = Math.floor(Math.random() * 256);
+  let RGBcolor = "rgb(" + x + ',' + y + ',' + z + ")"
+  console.log(RGBcolor)
+
+}
+
+
 
