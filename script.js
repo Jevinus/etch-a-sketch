50ecb4caf386370c805299f71ab275b3e8454fdd
Jevinus <jevinus@protonmail.com>
2024-03-14 10:55:24 -0400

    Added reset button

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // Create a button allowing for the change of grid color using RGB
 // Add an onclick for the grid divs that changes individual its color 
 
-// Dynamic grid function allowing for 
+// Dynamic grid function
 function grid() {
   let container = document.getElementById('grid_container');
   let gridInput = prompt("Enter grid size:");
@@ -11,18 +11,16 @@ function grid() {
   for (let i = 0; i <= gridInput; i++) {
     let grid = document.createElement('div');
     grid.className = 'grid';
-    grid.style.backgroundColor = 'white';
-    grid.style.border = '1px solid black';
-    grid.style.overflow = 'hidden';
     container.appendChild(grid);
   }
 
   container.appendChild(grid_container);
 }
 
-// Button to change grid size
+// Grid size button
 let gridButton = document.getElementsByClassName('grid_selector')[0];
 gridButton.addEventListener('click', grid)
+
 // RGB value generator for grid cells
 function gridRGB() {
   const x = Math.floor(Math.random() * 256);
@@ -30,8 +28,17 @@ function gridRGB() {
   const z = Math.floor(Math.random() * 256);
   let RGBcolor = "rgb(" + x + ',' + y + ',' + z + ")"
   console.log(RGBcolor)
-
 }
 
+// Reset grid
+let clearButton = document.getElementsByClassName('grid_selector')[1];
+clearButton.addEventListener('click', function() {
+  gridInput = 0; 
+  let container = document.getElementById('grid_container');
+  container.innerHTML = ''; 
+});
 
-
+let gridCells = document.getElementsByClassName('grid')
+gridCells.addEventListener('click', function() {
+  gridCells.style.backgroundcolor = gridRGB();
+});
